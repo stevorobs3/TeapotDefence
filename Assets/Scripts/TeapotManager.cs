50e6053cf02fb644376f38e5ad9324fa4269f381
Steve <steve.robinson290391@gmail.com>
2016-09-15 22:36:32 +0100

    Initial Commit
    Add initial teapot assets

---
+++ Assets/Scripts/TeapotManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TeapotManager : MonoBehaviour {
+
+    public GameObject _teapotPrefab;
+    private ParticleSystem _teapotSteam;
+
+
+    private GameObject _teapot;
+    void Awake()
+    {
+        _teapot = Instantiate(_teapotPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        _teapot.transform.SetParent(gameObject.transform);
+        _teapotSteam = GetComponentInChildren<ParticleSystem>();
+    }
+
+
+    void Update ()
+    {
+        FollowMouse();
+        Move();
+        FireWeapon();
+    }
+
+    const float MOVEMENT_SPEED = 0.05f;
+    const float COOLDOWN = 1f;
+
+    float? lastFire;
+
+    private void FireWeapon()
+    {
+        if (Input.GetMouseButton(0) && (lastFire == null || (Time.time - lastFire) > COOLDOWN))
+        {
+            _teapotSteam.Play();
+            lastFire = Time.time;
+        }
+    }
+
+    private void Move()
+    {
+        float deltaX = 0, deltaY = 0;
+        if (Input.GetKey(KeyCode.W))
+            deltaY += MOVEMENT_SPEED;
+        if (Input.GetKey(KeyCode.S))
+            deltaY -= MOVEMENT_SPEED;
+        if (Input.GetKey(KeyCode.A))
+            deltaX -= MOVEMENT_SPEED;
+        if (Input.GetKey(KeyCode.D))
+            deltaX += MOVEMENT_SPEED;
+        _teapot.transform.position += new Vector3(deltaX, deltaY, 0);
+    }
+
+    private void FollowMouse()
+    {
+        var cameraPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        cameraPosition.z = 0;
+
+        var dir = cameraPosition - transform.position;
+        var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        _teapot.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        var yScale = (cameraPosition.x > 0) ? 1 : -1;
+        _teapot.transform.localScale = new Vector3(-1, yScale, 1);
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TileManager : MonoBehaviour {
+
+    public GameObject GrassTile;
+
+    const int PIXELS_PER_TILE = 128;
+    const int NUM_TILES_WIDE = 25;
+
+    // Use this for initialization
+    void Awake () {
+        SpawnTiles();
+	}
+
+    private void SpawnTiles()
+    {
+        for (int i = -NUM_TILES_WIDE / 2; i < NUM_TILES_WIDE / 2; i++)
+        {
+            for (int j = -NUM_TILES_WIDE; j < NUM_TILES_WIDE / 2; j++)
+            {
+                var position = new Vector3(i, j,0);
+                var go = Instantiate(GrassTile, position, Quaternion.identity) as GameObject;
+                go.transform.SetParent(gameObject.transform, false);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
