eed21f363e012d3c84f37306c925622a7e6da580
Steve <steve.robinson290391@gmail.com>
2016-09-21 22:42:51 +0100

    Tweek Cafetiere spawn rate

--- Assets/Scripts/CoffeeMakerSpawner.cs
+++ Assets/Scripts/CoffeeMakerSpawner.cs
@@ -10,7 +10,9 @@ public class CoffeeMakerSpawner : MonoBehaviour {
 
     private float _timeBetweenSpawns = 5f;
     const float SPAWN_RATE_MULTIPLIER = 1.5f;
-    const int   SPAWN_RATE_INCREASE = 10;
+
+    const int SPAWN_RATE_INCREASE = 10;
+    const int MAX_SPAWN_RATE_INCREASE = 30;
 
     private float? lastSpawnTime = null;
     const float SPAWN_DISTANCE_X = 5.5f;
@@ -46,6 +48,7 @@ public class CoffeeMakerSpawner : MonoBehaviour {
         if (_spawnCount % SPAWN_RATE_INCREASE == 0)
         {
             _timeBetweenSpawns /= SPAWN_RATE_MULTIPLIER;
+            _timeBetweenSpawns = Mathf.Min(MAX_SPAWN_RATE_INCREASE, _timeBetweenSpawns);
             Debug.Log("Increasing spawn rate to " + _timeBetweenSpawns + " after " + _spawnCount + " spawns");
         }
         var randomIndex = new System.Random().Next(_spawnPoints.Count);
