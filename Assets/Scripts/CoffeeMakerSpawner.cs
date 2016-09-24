eedcf804ea9c057a0fe8af62cd39201b397b5c7f
Steve <steve.robinson290391@gmail.com>
2016-09-24 10:29:10 +0100

    Make Cafetiere spawning more random

--- Assets/Scripts/CoffeeMakerSpawner.cs
+++ Assets/Scripts/CoffeeMakerSpawner.cs
@@ -6,28 +6,34 @@ public class CoffeeMakerSpawner : MonoBehaviour {
 
     private List<Vector3> _spawnPoints = new List<Vector3>();
 
-    public GameObject Cafetiere;
+    public GameObject _cafetiere;
 
-    private float _timeBetweenSpawns = 5f;
-    const float SPAWN_RATE_MULTIPLIER = 1.5f;
+    private float _timeBetweenSpawns;
+    private float _minTimeBetweenSpawns = 5f;
+    private float _maxTimeBetweenSpawns = 7f;
+
+    const float SPAWN_RATE_MULTIPLIER = 1.25f;
 
     const int SPAWN_RATE_INCREASE = 10;
-    const int MAX_SPAWN_RATE_INCREASE = 30;
+    const float MAX_SPAWN_RATE = 0.2f;
 
-    private float? lastSpawnTime = null;
+    private float? _lastSpawnTime = null;
     const float SPAWN_DISTANCE_X = 5.5f;
     const float SPAWN_DISTANCE_Y = 4.5f;
 
+    const int MIN_SPAWNS = 1;
+    const int MAX_SPAWNS = 2;
+
     private int _spawnCount;
+
+    System.Random _randomGenerator;
 
     // Use this for initialization
     void Start()
     {
-        _spawnCount = 0;
-        _spawnPoints.Add(new Vector3(-SPAWN_DISTANCE_X, -SPAWN_DISTANCE_Y));
-        _spawnPoints.Add(new Vector3(SPAWN_DISTANCE_X, -SPAWN_DISTANCE_Y));
-        _spawnPoints.Add(new Vector3(SPAWN_DISTANCE_X, SPAWN_DISTANCE_Y));
-        _spawnPoints.Add(new Vector3(-SPAWN_DISTANCE_X, SPAWN_DISTANCE_Y));
+        _randomGenerator = new System.Random();
+        NextTimeBetweenSpawns();
+        _lastSpawnTime = Time.time;
     }
 
 
@@ -35,24 +41,64 @@ public class CoffeeMakerSpawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (lastSpawnTime == null || Time.time - lastSpawnTime > _timeBetweenSpawns)
+        if (_lastSpawnTime == null || Time.time - _lastSpawnTime > _timeBetweenSpawns)
         {
-            lastSpawnTime = Time.time;
-            SpawnCoffeeMaker();
+            NextTimeBetweenSpawns();
+            _lastSpawnTime = Time.time;
+
+            int numSpawns = _randomGenerator.Next(MIN_SPAWNS, MAX_SPAWNS + 1);
+            SpawnCoffeeMakers(numSpawns);
         }
 	}
+
+    private void SpawnCoffeeMakers(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            SpawnCoffeeMaker();
+        }
+    }
 
     private void SpawnCoffeeMaker()
     {
         _spawnCount++;
         if (_spawnCount % SPAWN_RATE_INCREASE == 0)
         {
-            _timeBetweenSpawns /= SPAWN_RATE_MULTIPLIER;
-            _timeBetweenSpawns = Mathf.Min(MAX_SPAWN_RATE_INCREASE, _timeBetweenSpawns);
-            Debug.Log("Increasing spawn rate to " + _timeBetweenSpawns + " after " + _spawnCount + " spawns");
+            _minTimeBetweenSpawns /= SPAWN_RATE_MULTIPLIER;
+            _maxTimeBetweenSpawns /= SPAWN_RATE_MULTIPLIER;
+            _minTimeBetweenSpawns = Mathf.Max(MAX_SPAWN_RATE, _minTimeBetweenSpawns);
+            _maxTimeBetweenSpawns = Mathf.Max(MAX_SPAWN_RATE, _maxTimeBetweenSpawns);
         }
-        var randomIndex = new System.Random().Next(_spawnPoints.Count);
-        var coffeeMaker = Instantiate(Cafetiere, _spawnPoints[randomIndex], Quaternion.identity) as GameObject;
+        Vector3 spawnPoint = RandomSpawnPoint();
+        var coffeeMaker = Instantiate(_cafetiere, spawnPoint, Quaternion.identity) as GameObject;
         coffeeMaker.transform.SetParent(transform);
+    }
+
+    private void NextTimeBetweenSpawns()
+    {
+        _timeBetweenSpawns = _randomGenerator.NextFloat(_minTimeBetweenSpawns, _maxTimeBetweenSpawns);
+    }
+
+
+    private Vector3 RandomSpawnPoint()
+    {
+        if (_randomGenerator.NextBoolean())
+        {
+            bool isRight = _randomGenerator.NextBoolean();
+            float x = SPAWN_DISTANCE_X * (isRight ? -1 : 1);
+            
+            float y = _randomGenerator.NextFloat(-SPAWN_DISTANCE_Y, SPAWN_DISTANCE_Y);
+            Debug.Log("Spawning on " + (isRight ? "left" : "right") + " at y " + y);
+            return new Vector3(x, y);
+        }
+        else
+        {
+            bool isTop = _randomGenerator.NextBoolean();
+            float y = SPAWN_DISTANCE_Y * (isTop ? 1 : -1);
+
+            float x = _randomGenerator.NextFloat(-SPAWN_DISTANCE_X, SPAWN_DISTANCE_X);
+            Debug.Log("Spawning on " + (isTop ? "top" : "bottom") + " at x " + x);
+            return new Vector3(x, y);
+        }
     }
 }
---
+++ Assets/Scripts/RandomExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public static class RandomExtension
+{
+    /// <summary>
+    /// Return a random boolean with probability of being true equal to p
+    /// </summary>
+    /// <param name="random"></param>
+    /// <param name="p"></param>
+    /// <returns></returns>
+    public static bool NextBoolean(this Random random, float p = 0.5f)
+    {
+        int num = random.Next();
+        return num < Int32.MaxValue * p;
+    }
+
+    public static float NextFloat(this Random random, float min, float max)
+    {
+        double num = random.NextDouble();
+        return (float)(min + (max - min) * num);
+    }
+}
