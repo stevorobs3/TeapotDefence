99305df68366b8ffd215ce1194f24953154c37bb
Steve <steve.robinson290391@gmail.com>
2016-10-03 22:06:29 +0100

    Increase time between spawns

--- Assets/Scripts/SpawnWave.cs
+++ Assets/Scripts/SpawnWave.cs
@@ -1,11 +1,11 @@
 ﻿
 public class SpawnWave
 {
-    private static readonly SpawnWave Level0 = new SpawnWave(10, 1, 5f, 3f);
-    private static readonly SpawnWave Level1 = new SpawnWave(10, 2, 5f, 2.5f);
-    private static readonly SpawnWave Level2 = new SpawnWave(20, 5, 4f, 2.25f);
-    private static readonly SpawnWave Level3 = new SpawnWave(15, 10, 3f, 2f);
-    private static readonly SpawnWave Level4 = new SpawnWave(10, 15, 3f, 1.5f);
+    private static readonly SpawnWave Level0 = new SpawnWave(10, 1, 2f, 3f);
+    private static readonly SpawnWave Level1 = new SpawnWave(10, 2, 10f, 2.5f);
+    private static readonly SpawnWave Level2 = new SpawnWave(20, 5, 10f, 2.25f);
+    private static readonly SpawnWave Level3 = new SpawnWave(15, 10, 10f, 2f);
+    private static readonly SpawnWave Level4 = new SpawnWave(10, 15, 10f, 1.5f);
     public static readonly SpawnWave[] Levels = new SpawnWave[]
     {
         Level0,
