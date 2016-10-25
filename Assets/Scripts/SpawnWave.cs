df9416e52ed1a1882218c52528af753edf3314ba
Steve <steve.robinson290391@gmail.com>
2016-10-25 21:31:47 +0100

    Add extra levels

--- Assets/Scripts/SpawnWave.cs
+++ Assets/Scripts/SpawnWave.cs
@@ -6,6 +6,11 @@ public class SpawnWave
     private static readonly SpawnWave Level2 = new SpawnWave(20, 5, 1f, 2.25f);
     private static readonly SpawnWave Level3 = new SpawnWave(15, 10, 1f, 2f);
     private static readonly SpawnWave Level4 = new SpawnWave(10, 15, 1f, 1.5f);
+    private static readonly SpawnWave Level5 = new SpawnWave(15, 15, 1f, 1.4f);
+    private static readonly SpawnWave Level6 = new SpawnWave(20, 20, 1f, 1.3f);
+    private static readonly SpawnWave Level7 = new SpawnWave(25, 25, 1f, 1.2f);
+    private static readonly SpawnWave Level8 = new SpawnWave(30, 30, 1f, 1.1f);
+
     public static readonly SpawnWave[] Levels = new SpawnWave[]
     {
         Level0,
@@ -13,6 +18,10 @@ public class SpawnWave
         Level2,
         Level3,
         Level4,
+        Level5,
+        Level6,
+        Level7,
+        Level8,
     };
 
     private SpawnWave(
